80fbd9178746971db9d0b9cffc0ee73a2b3b3e3b
ishaan-ansari <ishaanansari044@gmail.com>
2026-06-04 14:24:50 +0530

    Implement DFS traversal algorithm with adjacency list representation

---
+++ 03_DFS_traversal.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class DFS:
+    def __init__(self, adj_list):
+        self.adj_list = adj_list
+
+    def traverse(self, start):
+        visited = [0]*len(self.adj_list)
+        self._dfs_helper(start, visited)
+
+    def _dfs_helper(self, node, visited):
+        if not visited[node]:
+            print(node, end=' ')
+            visited[node] = 1
+            for neighbor in self.adj_list[node]:
+                if not visited[neighbor]:
+                    self._dfs_helper(neighbor, visited)
+
+edges = [[1,0], [0,2], [0,5], [3,2], [3,4]]
+
+# Create an adjacency list
+adj_list = defaultdict(list)
+for u, v in edges:
+    adj_list[u].append(v)
+    adj_list[v].append(u)
+
+obj = DFS(adj_list)
+print("DFS Traversal starting from node 0:")
+obj.traverse(0)
