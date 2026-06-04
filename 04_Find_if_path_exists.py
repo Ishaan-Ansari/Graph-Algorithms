29906a857380ed5c7ac2d52885f8e9cbe9b5faec
ishaan-ansari <ishaanansari044@gmail.com>
2026-06-04 14:26:13 +0530

    Implement validPath function to determine if a path exists in a graph

---
+++ 04_Find_if_path_exists.py
@@ -0,0 +1,28 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        adj_lst = defaultdict(list)
+
+        # create a adjacency list
+        for u, v in edges:
+            adj_lst[u].append(v)
+            adj_lst[v].append(u)
+
+
+        q = deque([source])
+        visited = set([source])
+
+        while q:
+            node = q.popleft()
+
+            if node == destination:
+                return True
+
+            for neighbor in adj_lst[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    q.append(neighbor)
+
+        
+        return False
