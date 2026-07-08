914190468d4f645a2d466c143bc6cefaa8914afe
ishaan-ansari <ishaanansari044@gmail.com>
2026-07-08 14:17:37 +0530

    Refactor adjacency list creation and visited tracking in path existence check

--- 04_Find_if_path_exists.py
+++ 04_Find_if_path_exists.py
@@ -2,27 +2,23 @@ from collections import defaultdict, deque
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # 1. Create a adjecency list
         adj_lst = defaultdict(list)
-
-        # create a adjacency list 
         for u, v in edges:
             adj_lst[u].append(v)
             adj_lst[v].append(u)
 
-
+        visited = [0]*n
         q = deque([source])
-        visited = set([source])
 
         while q:
             node = q.popleft()
-
             if node == destination:
                 return True
 
             for neighbor in adj_lst[node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
+                if not visited[neighbor]:
+                    visited[neighbor] = 1
                     q.append(neighbor)
-
-        
+                    
         return False
